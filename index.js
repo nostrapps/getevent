bf659279b37004a4e57fa9c7b63e544796100f9f
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-15 13:54:41 +0200

    event structure

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const path = require('path')
 // Get events from command line arguments
 const events = process.argv.slice(2)
 if (events.length === 0) {
-  events.push('1555d356baf4ca4773ff1ce6c1a2da75d16087b2329a981b30268e3507c76f91')
+	events.push('1555d356baf4ca4773ff1ce6c1a2da75d16087b2329a981b30268e3507c76f91')
 }
 
 const root = './.well-known/nostr/event'
 
 events.forEach(event => {
-  const cacheDirectory = path.join(root, `${event}`)
-  const filename = 'index.json'
-  const cacheFilePath = path.join(cacheDirectory, filename)
+	const cacheDirectory = path.join(root, `${event}`)
+	const filename = 'index.json'
+	const cacheFilePath = path.join(cacheDirectory, filename)
 
-  fs.mkdirSync(cacheDirectory, { recursive: true })
+	fs.mkdirSync(cacheDirectory, { recursive: true })
 })
 
 const scsi = 'wss://relay.damus.io/'
@@ -25,20 +25,28 @@ const relay = [scsi]
 
 const pool = RelayPool(relay)
 pool.on('open', relay => {
-  // Subscribe for each event
-  events.forEach(event => {
-    relay.subscribe('subid' + event, { ids: [event] })
-  })
+	// Subscribe for each event
+	events.forEach(event => {
+		relay.subscribe('subid' + event, { ids: [event] })
+	})
 })
 
 pool.on('event', (relay, sub_id, ev) => {
-  console.log('event', sub_id, ev)
-  const event = sub_id.slice(5) // Remove 'subid' prefix to get the event
-  const cacheDirectory = path.join(root, `${event}`)
-  const filename = 'index.json'
-  const cacheFilePath = path.join(cacheDirectory, filename)
+	console.log('event', sub_id, ev)
+	const event = sub_id.slice(5) // Remove 'subid' prefix to get the event
+	const cacheDirectory = path.join(root, `${event}`)
+	const filename = 'index.json'
+	const cacheFilePath = path.join(cacheDirectory, filename)
 
-  fs.writeFileSync(cacheFilePath, JSON.stringify(ev, null, 2))
+	const out = {
+		'@id': '',
+		mainEntity: {
+			'@id': 'nostr:event:' + event,
+			...ev
+		}
+	}
 
-  relay.close()
+	fs.writeFileSync(cacheFilePath, JSON.stringify(out, null, 2))
+
+	relay.close()
 })
